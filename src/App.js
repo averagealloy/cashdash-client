85bc11417388b296090d6273d579b379bbc5678c
averagealloy <m.mctighe10@gmail.com>
2020-02-23 14:52:31 -0500

    getting react, reacr-redux, redux-thunk, react-router-dom installed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from 'react';
 
 class App extends React.Component {
 
-  componentDidMount(){
-    fetch('http://localhost:3001/api/v1/accounts', {
-      method: 'GET'
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }
+  // componentDidMount(){
+  //   fetch('http://localhost:3001/api/v1/accounts', {
+  //     method: 'GET'
+  //   })
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }
 
   render() {
   return (
