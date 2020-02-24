e1290ae510bfb65e0afc0492daf16d89bf6bd658
averagealloy <m.mctighe10@gmail.com>
2020-02-24 14:41:05 -0500

    controlled form for account input

--- src/components/AccountInput.js
+++ src/components/AccountInput.js
@@ -3,10 +3,28 @@ import React from 'react'
 
 class AccountInput extends React.Component {
 
+    state = {
+        name:'',
+        balance: ''
+    }
+
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+
     render() {
         return (
             <div>
-                Account Input
+                <form>
+                    <label>Checking Account Name:</label>
+                    <input type='text' placeholder='Name' value={this.state.name} name='name' onChange={this.handleChange}/><br/>
+                    <label>Checking Account Balance:</label>
+                    <input type='text' placeholder='Balance' value={this.state.balance} name='balance' onChange={this.handleChange}/>
+                </form>
             </div>
         )
     }
