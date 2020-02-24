1414961601a8ba9242ea1fc6f56170d2c50dbeb2
averagealloy <m.mctighe10@gmail.com>
2020-02-24 14:49:16 -0500

    I know that the debugger is still in there but had to commit bc I added add acount

--- src/components/AccountInput.js
+++ src/components/AccountInput.js
@@ -15,15 +15,20 @@ class AccountInput extends React.Component {
         })
     }
 
+    handleSubmit = () => {
+        debugger;
+    }
+
 
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>Checking Account Name:</label>
                     <input type='text' placeholder='Name' value={this.state.name} name='name' onChange={this.handleChange}/><br/>
                     <label>Checking Account Balance:</label>
-                    <input type='text' placeholder='Balance' value={this.state.balance} name='balance' onChange={this.handleChange}/>
+                    <input type='text' placeholder='Balance' value={this.state.balance} name='balance' onChange={this.handleChange}/><br/>
+                    <input type='submit'/>
                 </form>
             </div>
         )
