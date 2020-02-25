60196a965b5960f7f67a94940b1c4b39930d6176
averagealloy <m.mctighe10@gmail.com>
2020-02-25 18:06:13 -0500

    the transactions are showing up with all the right info

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Transactions = (props) => {
     return(
         <div>
-            Transactions
+            {props.transactions && props.transactions.map( transaction => 
+                <li key={transaction.id}>{transaction.kind} - {transaction.amount} - {transaction.date}</li>
+            )}
         </div>
     )
 }
