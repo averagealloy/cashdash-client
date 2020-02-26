83ff3ba28315cabcc217820069d171ec3a79683d
averagealloy <m.mctighe10@gmail.com>
2020-02-26 13:29:24 -0500

    just trying to get the api comuncating w the react app using debugger to help me out

---
+++ src/actions/deleteTransaction.js
@@ -0,0 +1,11 @@
+export const deleteTransaction = (transactionId, accountId) => {
+
+    return dispatch => {
+        return fetch(`http://localhost:3001/api/v1/accounts/${accountId}/transactions/${transactionId}`, {
+            method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(account => dispatch({type: 'DELETE_TRANSACTION', payload: account}))
+    }
+
+}
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {deleteTransaction} from '../actions/deleteTransaction'
 
 const Transactions = (props) => {
+
+const handleDelete = (transaction) => {
+    
+    props.deleteTransaction(transaction.id, transaction.account_id)
+}
+
     return(
         <div>
             {props.transactions && props.transactions.map( transaction => 
-                <li key={transaction.id}>{transaction.kind} - {transaction.amount} </li>
+                <li key={transaction.id}>{transaction.kind} - {transaction.amount}<button onClick={() => handleDelete(transaction)}>Delete</button> </li>
             )}
         </div>
     )
 }
 
-export default Transactions
+export default connect(null, {deleteTransaction})(Transactions)
