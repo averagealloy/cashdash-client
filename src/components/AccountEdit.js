8ff0447771755a57a90af9466f7185928cc4a95f
averagealloy <m.mctighe10@gmail.com>
2020-02-26 15:06:14 -0500

    update accout label and removed balance

--- src/components/AccountEdit.js
+++ src/components/AccountEdit.js
@@ -30,11 +30,11 @@ class AccountEdit extends React.Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Checking Account Name:</label>
+                <form onSubmit={this.handleSubmit}><br/><br/>
+                    <label>Update your Account Name:</label>
                     <input type='text' placeholder='Name' value={this.state.name} name='name' onChange={this.handleChange}/><br/>
-                    <label>Checking Account Balance:</label>
-                    <input type='text' placeholder='Balance' value={this.state.balance} name='balance' onChange={this.handleChange}/><br/>
+                    
+                    <br/>
                     <input type='submit'/>
                 </form>
             </div>
