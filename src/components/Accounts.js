139db5748c0a864b6cbbbb641990756f2ab40915
averagealloy <m.mctighe10@gmail.com>
2020-03-05 14:52:10 -0500

    took out some whote space

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const Accounts = (props) => {
+    console.log(props)
     return(
         <div>
             <h1>Accounts Page</h1>
